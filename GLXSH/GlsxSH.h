9c3ac4e89688ff846e976d6a68da37dbad4be946
rahat.adnan <sthep90@Gmail.com>
2025-06-06 11:43:41 +0600

    glsx update

--- GLXSH/GlsxSH.h
+++ GLXSH/GlsxSH.h
@@ -1,8 +1,17 @@
 ﻿#pragma once
+#include <string>
 
 class GlsxSH{
 private:
     unsigned int program;
+    unsigned int vertexShader;
+    unsigned int fragmentShader;
+    std::string vertexSource;
+    std::string fragmentSource;
+    std::string loadDirectory;
 public:
+    GlsxSH();
+    ~GlsxSH();
+    void loadShader();
     
 };
